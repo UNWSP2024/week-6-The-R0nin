70e0e923801c6559ac2cbdd804add0bef3a925eb
The-R0nin <2025esteele@isd2144.org>
2024-10-11 10:56:44 -0500

    Update program_3.py
    Ethan Steele
    10/11
    Done

--- program_3.py
+++ program_3.py
@@ -4,6 +4,18 @@
 # The state sales tax rate is 5 percent and the county sales tax rate is 2.5 percent.  
 # Write a program that asks the user to enter the total sales for the month.  
 # From this figure, the application should calculate and display the following:
+Sales_Tax = 0.05
+Country_Sales_Tax = 0.025
+
+monthy_sale = float(input('List sale for the month: $'))
+
+sales_price = monthy_sale * Sales_Tax
+country_sales_price = monthy_sale * Country_Sales_Tax
+total = sales_price + country_sales_price
+
+print('With sales tax: $', float(sales_price))
+print('With country sales tax: $', float(country_sales_price))
+print('Total: $', float(total))
 
 # The amount of county sales tax.
 # The amount of state sales tax.
